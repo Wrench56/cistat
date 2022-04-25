52e8187c5ff046bb1d00deed98cf836e0f4a6782
Wrench56 <dmarkreg@gmail.com>
2022-04-25 23:55:02 +0200

    Webview functionality added

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cistat" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('cistat', async () => {
+		console.log('[+] Start');
+		
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        'cistat',
+        'Cistat Statistics',
+        vscode.ViewColumn.One,
+        {}
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cistat.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cistat!');
-	});
-
-	context.subscriptions.push(disposable);
+      // And set its HTML content from the file
+      panel.webview.html = await generateHTML();
+	  
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+async function generateHTML() {
+	let html = '';
+	let css = '';
+
+	await fs.promises.readFile(__dirname + '/../src/webview/index.html', 'utf-8').then(data => html = data);
+	await fs.promises.readFile(__dirname + '/../src/webview/index.css', 'utf-8').then(data => css = data);
+	
+	html = html.replace('<!--${index.css}-->', '<style>\n'+css+'</style>\n');
+
+	return html;
+}
+
+	
