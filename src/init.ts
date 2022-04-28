c685658006e1de3bca318191d307db65fec3b22f
Wrench56 <dmarkreg@gmail.com>
2022-04-28 23:29:44 +0200

    Added line counting and event system

--- src/init.ts
+++ src/init.ts
@@ -1,21 +1,39 @@
-import { getLines } from './functions/get_lines';
+import { getFileContent} from './providers/files';
 import { getWorkingDir, generateHTML } from './utils';
 import { parse } from 'node-html-parser';
+import * as lines from './functions/lines';
+import * as vscode from 'vscode';
 
-export async function init() {
+export async function init(panel: vscode.WebviewPanel) {
     const html = parse(await generateHTML()); // parse to use the .getElementById etc.
 
-    const dir = getWorkingDir();
+    let dir = getWorkingDir();
     if (dir === false) {
         // Not yet supported...
         html.getElementById('status')!.innerHTML = 'This VSCode window did not open a project!';
         return html.toString();
     } else {
+        dir += '/';
         html.getElementById('status')!.innerHTML = 'Success!';
     }
 
-    getLines();
+    getFileContent(dir).then(content => {
+        saveFileEvents(content, panel);
+    });
+
+    vscode.workspace.onDidSaveTextDocument((document) => {
+        dir = getWorkingDir();
+        getFileContent(dir).then(content => {
+            saveFileEvents(content, panel);
+        });
+    });
 
 
     return html.toString(); // vscode needs a string
+}
+
+function saveFileEvents(content: Map<string, string> | false, panel: vscode.WebviewPanel) {
+    if (content !== false) {
+        lines.handler(content, panel);
+    }
 }
